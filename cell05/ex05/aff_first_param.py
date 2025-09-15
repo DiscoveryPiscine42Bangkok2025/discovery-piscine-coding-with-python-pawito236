58c661375ad6ac31d6c197a1b86c03b604a7210f
pawito236 <pawito236@gmail.com>
2025-09-15 21:32:22 +0700

    chore: remove comment

--- cell05/ex05/aff_first_param.py
+++ cell05/ex05/aff_first_param.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-
-# Check the number of arguments passed to the script
 if len(sys.argv) > 1:
-    # If there is at least one parameter, print the first one
     print(sys.argv[1])
 else:
-    # If there are no parameters, print "none"
     print("none")
