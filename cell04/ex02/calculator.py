f8526ae6450aae22270866969799923cc31713a5
pawito236 <pawito236@gmail.com>
2025-09-15 17:30:09 +0700

    cell04: ex02

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+num1 = float(input("Give me the first number: "))
+num2 = float(input("Give me the second number: "))
+
+print("Thank you!")
+print(f"{int(num1)} + {int(num2)} = {int(num1 + num2)}")
+print(f"{int(num1)} - {int(num2)} = {int(num1 - num2)}")
+print(f"{int(num1)} / {int(num2)} = {int(num1 / num2)}")
+print(f"{int(num1)} * {int(num2)} = {int(num1 * num2)}")
